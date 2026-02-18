fbb42fe50720b9b51a968850ce1c62274d632e52
almeida <hello@eualmeida.com.br>
2026-02-18 15:29:39 -0300

    feat(update): add upgrade alias and remove unnecessary runUpdate func

--- cmd/update.go
+++ cmd/update.go
@@ -7,18 +7,13 @@ import (
 
 var updateCmd = &cobra.Command{
 	Use:   "update",
+	Aliases: []string{"upgrade"},
 	Short: "Update Rewrite CLI to the latest version",
-	RunE:  runUpdate,
-}
-
-func runUpdate(cmd *cobra.Command, _ []string) error {
-	noColor, err := cmd.Flags().GetBool("no-color")
-
-	if err != nil {
-		return err
-	}
-
-	return update.Update(noColor)
+	RunE:  func(cmd *cobra.Command, args []string) error {
+		noColor, _ := cmd.Flags().GetBool("no-color")
+		
+		return update.Update(noColor)
+	},
 }
 
 func init() {
